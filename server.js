04ce51c728495616dcbe9b90fe0029cbbeeae430
dantemessy <thepunishar1@gmail.com>
2019-12-30 15:59:46 +0200

    finished the first feature on trello

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+
+const express = require('express') ;
+const app = express() ;
+const PORT = process.env.PORT || 3333 ;
+const superagent = require('superagent') ;
+
+require('dotenv').config() ;
+
+app.set('view engine' , 'ejs') ;
+
+app.listen(PORT, () => console.log('first Baby Step'));
+
+app.get('/' , (req , res) => {
+  res.status(200).send('Alrrrrrrrright !!')
+});
+
+
+
+//link the css file
+//thank you ayman
+app.use( '/public' , express.static( 'public'));
+
+
+
+// app.use(express.static('public'));
+
+//test our index !!
+app.get( '/test' , (req , res) => {
+  res.render('pages/index') ;
+})
+
+
+// errors
+app.get('*' , (req , res) => {
+  res.status(404).send('Ooops 404 !!!')
+})
+
+
